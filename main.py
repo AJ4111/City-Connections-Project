cd72aac8d875a87b8694c8dcf6e58ee264c58f7b
Laura Ozoria <101300098+carrotpiex@users.noreply.github.com>
2026-05-01 17:20:08 -0400

    clean main

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from kruskal import kruskal
 from prim import prim
 import sys
 
-import time
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
@@ -11,17 +10,9 @@ output_file = sys.argv[2]
 edges, adj = read_graph(input_file)
 nodes = set(adj.keys())
 
-# timing of algorithms
-start = time.time()
 mst = kruskal(edges, nodes)
-kruskal_time = time.time() - start
+# mst = prim(nodes, adj)
 
-start = time.time()
-mst = prim(nodes, adj)
-prim_time = time.time() - start
-
-print(f"kruskal: {kruskal_time * 1000:4f} ms")
-print(f"prim: {prim_time * 1000:4f} ms")
 
 with open(output_file, 'w') as f:
     for edge_id, u, v, w in mst:
